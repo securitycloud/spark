54c49580f819c9280e0b09e1812d9bb44deeea08
Martin Jelinek <mjelinek@barinka.net>
2015-06-13 19:07:29 +0200

    * removed lambda functions to remove repeated OutputProducer instance creation * 3 stream threads, testing code with different approaches

--- src/main/java/cz/muni/fi/spark/App.java
+++ src/main/java/cz/muni/fi/spark/App.java
@@ -5,15 +5,16 @@ import cz.muni.fi.json.JSONFlattener;
 import cz.muni.fi.kafka.OutputProducer;
 import cz.muni.fi.util.PropertiesParser;
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.function.Function;
+import org.apache.spark.api.java.function.VoidFunction;
 import org.apache.spark.streaming.Durations;
-import org.apache.spark.streaming.api.java.JavaPairReceiverInputDStream;
+import org.apache.spark.streaming.api.java.JavaPairDStream;
 import org.apache.spark.streaming.api.java.JavaStreamingContext;
 import org.apache.spark.streaming.kafka.KafkaUtils;
 import scala.Tuple2;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Properties;
+import java.util.*;
 
 
 /**
@@ -22,6 +23,9 @@ import java.util.Properties;
 public class App {
 
     static final long BATCH_INTERVAL = 1000;
+    
+    public static final JSONFlattener jsonFlattener = new JSONFlattener(new ObjectMapper());
+    public static final OutputProducer prod = new OutputProducer();
 
     public static void main(String[] args) {
         final SparkConf sparkConf = getSparkConf();
@@ -30,25 +34,33 @@ public class App {
 
         Map<String, Integer> topicMap = new HashMap<>();
         topicMap.put(kafkaProps.getProperty("consumer.topic"), 1); // topic, numThreads
+        
+        //Set<String> topicSet = new HashSet<>();
+        //topicSet.add(kafkaProps.getProperty("consumer.topic")); // topic
 
-        JavaPairReceiverInputDStream<String, String> messages =
-                KafkaUtils.createStream(jssc, kafkaProps.getProperty("zookeeper.url"), "1", topicMap);
+        //Map<String, String> kafkaPropsMap = new HashMap<String, String>();
+        //for (String key : kafkaProps.stringPropertyNames()) {
+        //    kafkaPropsMap.put(key, kafkaProps.getProperty(key));
+        //}
 
-        final JSONFlattener jsonFlattener = new JSONFlattener(new ObjectMapper());
+        int numStreams = 3;
+        List<JavaPairDStream<String, String>> kafkaStreams = new ArrayList<>(numStreams);
+        for (int i = 0; i < numStreams; i++) {
+            // standard basic stream creation
+            kafkaStreams.add(KafkaUtils.createStream(jssc, kafkaProps.getProperty("zookeeper.url"), "1", topicMap));
+            
+            // advanced stream creation with kafka properties as parameter
+            //kafkaStreams.add(KafkaUtils.createStream(jssc, String.class, String.class, StringDecoder.class, StringDecoder.class,
+            //        kafkaPropsMap, topicMap, StorageLevel.MEMORY_AND_DISK_SER_2()));
+            
+            // direct stream - new approach test
+            //kafkaStreams.add(KafkaUtils.createDirectStream(jssc, String.class, String.class, StringDecoder.class, StringDecoder.class, kafkaPropsMap, topicSet));
+        } 
+        
+        JavaPairDStream<String, String> messages = jssc.union(kafkaStreams.get(0), kafkaStreams.subList(1, kafkaStreams.size()));
+        
         // Each streamed input batch forms an RDD
-        messages.foreachRDD(rdd -> {
-            rdd.foreachPartition(it -> {
-                final OutputProducer prod = new OutputProducer();
-                while (it.hasNext()) {
-                    Tuple2<String, String> msg = it.next();
-                    Map<String, Object> json = jsonFlattener.jsonToFlatMap(msg._2());
-                    if (json.get("dst_port").equals(80)) { // filter by
-                        prod.sendJson(new Tuple2<>(null, json));
-                    }
-                }
-            });
-            return null;
-        });
+        messages.foreachRDD(new FunctionImpl());
 
         jssc.start();
         jssc.awaitTermination();
@@ -62,5 +74,34 @@ public class App {
                 .setMaster(sparkProps.getProperty("spark.master.url"))
                 .set("spark.executor.memory", sparkProps.getProperty("spark.executor.memory"))
                 .set("spark.serializer", sparkProps.getProperty("spark.serializer"));
+    }
+
+    private static class FunctionImpl implements Function<JavaPairRDD<String, String>, Void> {
+
+        public FunctionImpl() {
+        }
+
+        @Override
+        public Void call(JavaPairRDD<String, String> rdd) throws Exception {
+            rdd.foreachPartition(new VoidFunctionImpl());
+            return null;
+        }
+
+        private static class VoidFunctionImpl implements VoidFunction<Iterator<Tuple2<String, String>>> {
+
+            public VoidFunctionImpl() {
+            }
+
+            @Override
+            public void call(Iterator<Tuple2<String, String>> it) throws Exception {
+                while (it.hasNext()) {
+                    Tuple2<String, String> msg = it.next();
+                    Map<String, Object> json = jsonFlattener.jsonToFlatMap(msg._2());
+                    if (json.get("dst_port").equals(80)) { // filter by
+                        prod.sendJson(new Tuple2<>(null, json));
+                    }
+                }
+            }
+        }
     }
 }
