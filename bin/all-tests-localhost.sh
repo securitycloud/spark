e3fb11ac54efcb19230d5d82c59d3096be639ee5
halafi <filiphalas74@gmail.com>
2017-05-11 20:59:39 +0200

    refactoring

--- bin/all-tests-localhost.sh
+++ bin/all-tests-localhost.sh
@@ -1,25 +1,19 @@
 #!/bin/bash
-# script for testing on localhost/single machine
-# you need to set KAFKA_INSTALL (KAFKA_HOME) and SPARK_HOME to your installations of KAFKA/SPARK
+# script for testing on localhost / single machine
+# you need to set KAFKA_INSTALL (KAFKA_HOME) to your installations of KAFKA and adjust spark.home property in pom.xml
 # add delete.topic.enable=true to $KAFKA_INSTALL/config/server.properties, have zk and kafka running
 
-# master node
-ALL_SERVERS[1]=localhost
-NUMBER_OF_SLAVES=0
-
-# kafka
+# kafka home (spark home is set in pom.xml)
 KAFKA_INSTALL=/home/filip/kafka_2.11-0.8.2.2
-KAFKA_PRODUCER=localhost
-KAFKA_CONSUMER=localhost
 
-# spark
-export SPARK_HOME="/home/filip/spark-2.1.0-bin-hadoop2.7"
-
-TESTING_TOPIC=sparkOutput # should match kafka.producer.topic in pom.xml
-SERVICE_TOPIC=sparkResults
-
-# existing work directory folder on all machines
+# existing working directory
 WRK=/home/filip/spark-work-dir
+
+# input stream topic is only set as kafka.consumer.topic in pom.xml
+# topic for test output
+TESTING_TOPIC=sparkOutput # should match kafka.producer.topic in pom.xml
+# topic for test performance results
+SERVICE_TOPIC=sparkResults # should match kafka.producer.resultsTopic in pom.xml
 
 # console color switches
 ERR="\033[1;31m"
@@ -42,14 +36,15 @@ NUM_TESTS=$((NUM_TESTS * ${REPEAT}))
 # current test
 ACT_TEST=1
 
-echo -e $LOG Recreating output performance result topic$ERR $SERVICE_TOPIC$LOG on $KAFKA_CONSUMER $OFF
+echo -e $LOG Recreating output performance result topic$ERR $SERVICE_TOPIC$LOG on localhost $OFF
 $KAFKA_INSTALL/bin/kafka-topics.sh --delete --zookeeper localhost:2181 --topic $SERVICE_TOPIC > /dev/null
 $KAFKA_INSTALL/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic $SERVICE_TOPIC > /dev/null
-echo -e $LOG Recreating output test result topic$ERR $TESTING_TOPIC$LOG on $KAFKA_CONSUMER $OFF
+echo -e $LOG Recreating output test result topic$ERR $TESTING_TOPIC$LOG on localhost $OFF
 $KAFKA_INSTALL/bin/kafka-topics.sh --delete --zookeeper localhost:2181 --topic $TESTING_TOPIC > /dev/null
 $KAFKA_INSTALL/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic $TESTING_TOPIC > /dev/null
 
 
+# optional, only needed once
 if true;
     then
         echo  preparing working directory
@@ -67,7 +62,6 @@ if true;
         rm project.tar
         cd project
         mvn clean package -P local > /dev/null
-        # install spark
         echo -e $OK working dir ready $OFF
 fi
 
@@ -77,12 +71,11 @@ do
     do
         echo -e $LOG Running test $ACT_TEST/$NUM_TESTS: $OFF
         # submit app to spark-submit
-        cd ${WRK}
-        cd project
+        cd ${WRK}/project
         killall screen
         kill `ps aux | grep spark | grep -v grep | awk '{print $2}'`
         screen -S sparktest -d -m mvn exec:exec -Dspark.machines=3 -Dspark.testtype=$TEST -P local
-        echo -e ${OK} Test in progress... spark monitor at http://${ALL_SERVERS[1]}:4040
+        echo -e ${OK} Test in progress... spark monitor at http://localhost:4040
         # wait for test result message, to know the test has finished
         $KAFKA_INSTALL/bin/kafka-console-consumer.sh --zookeeper localhost:2181 --topic ${SERVICE_TOPIC} --max-messages 1
         # restart cluster
